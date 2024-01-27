b7ea8fb8a746b947e0762bafecbbcabc814ee2a1
MD. RAWHA SIDDIQI RIAD <84915274+RsRiad@users.noreply.github.com>
2024-01-27 13:47:25 +0600

    Basic Templet.cpp

---
+++ Basic Templet.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Macros
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define fl(i,n) for(int i=0;i<n;i++)
+#define rl(i,m,n) for(int i=n;i>=m;i--)
+#define yes cout<<"YES\n";
+#define endl "\n"
+#define no cout<<"NO\n";
+#define all(v) v.begin(),v.end()
+#define allr(v) v.rbegin(),v.rend()
+#define unq(x) x.erase(unique(x.begin(),x.end()),x.end())
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define RIAD ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+/**============================START============================**/
+
+//Code
+void solve()
+{
+    ll n;
+    cin>>n;
+    
+
+
+}
+//Main
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    file()
+    #endif
+    RIAD
+    ll t=1;
+    cin>>t;
+    fl(i,t)
+    {
+        solve();
+    }
+    return 0;
+}
+//End
