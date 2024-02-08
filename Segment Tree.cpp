aab6f29cb921dd28b9f1acf33f0a578333f955d7
MD. RAWHA SIDDIQI RIAD <84915274+RsRiad@users.noreply.github.com>
2024-02-08 23:33:04 +0600

    Segment Tree.cpp

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,81 +1,62 @@
-struct segmenttree {
-	int n;
-	vector<int> st;
+struct SegmentTree {
+    ll n;
+    vector<ll> st;
 
-	void init(int _n) {
-		this->n = _n;
-		st.resize(4 * n, 0);
-	}
+    void init(ll _n) {
+        n = _n;
+        st.resize(4 * n, 0);
+    }
 
-	void build(int start, int ending, int node, vector<int> &v) {
-		// leaf node base case
-		if (start == ending) {
-			st[node] = v[start];
-			return;
-		}
+    void build(ll node, ll s, ll e, vector<ll> &v) {
+        if (s == e) {
+            st[node] = v[s];
+            return;
+        }
+        ll mid = (s + e) >> 1;
+        ll l = node << 1;
+        ll r = l | 1;
+        build(l, s, mid, v);
+        build(r, mid + 1, e, v);
+        st[node] = st[l] + st[r];
+    }
 
-		int mid = (start + ending) / 2;
+    ll query(ll node, ll s, ll e, ll i, ll j) {
+        if (s > j || e < i) {
+            return 0;
+        }
+        if (s >= i && e <= j) {
+            return st[node];
+        }
+        ll mid = (s + e) >> 1;
+        ll l = node << 1;
+        ll r = l | 1;
+        ll q1 = query(l, s, mid, i, j);
+        ll q2 = query(r, mid + 1, e, i, j);
+        return q1 + q2;
+    }
 
-		// left subtree is (start,mid)
-		build(start, mid, 2 * node + 1, v);
+    void update(ll node, ll s, ll e, ll index, ll value) {
+        if (s == e) {
+            st[node] = value;
+            return;
+        }
+        ll mid = (s + e) >> 1;
+        ll l = node << 1;
+        ll r = l | 1;
+        if (index <= mid) update(l, s, mid, index, value);
+        else update(r, mid + 1, e, index, value);
+        st[node] = st[l] + st[r];
+    }
 
-		// right subtree is (mid+1,ending)
-		build(mid + 1, ending, 2 * node + 2, v);
+    void build(vector<ll> &v) {
+        build(1, 0, n - 1, v);
+    }
 
-		st[node] = st[node * 2 + 1] + st[node * 2 + 2];
-	}
+    ll query(ll l, ll r) {
+        return query(1, 0, n - 1, l, r);
+    }
 
-	int query(int start, int ending, int l, int r, int node) {
-		// non overlapping case
-		if (start > r || ending < l) {
-			return 0;
-		}
-
-		// complete overlap
-		if (start >= l && ending <= r) {
-			return st[node];
-		}
-
-		// partial case
-		int mid = (start + ending) / 2;
-
-		int q1 = query(start, mid, l, r, 2 * node + 1);
-		int q2 = query(mid + 1, ending, l, r, 2 * node + 2);
-
-		return q1 + q2;
-	}
-
-	void update(int start, int ending, int node, int index, int value) {
-		// base case
-		if (start == ending) {
-			st[node] = value;
-			return;
-		}
-
-		int mid = (start + ending) / 2;
-		if (index <= mid) {
-			// left subtree
-			update(start, mid, 2 * node + 1, index, value);
-		}
-		else {
-			// right
-			update(mid + 1, ending, 2 * node + 2, index, value);
-		}
-
-		st[node] = st[node * 2 + 1] + st[node * 2 + 2];
-
-		return;
-	}
-
-	void build(vector<int> &v) {
-		build(0, n - 1, 0, v);
-	}
-
-	int query(int l, int r) {
-		return query(0, n - 1, l, r, 0);
-	}
-
-	void update(int x, int y) {
-		update(0, n - 1, 0, x, y);
-	}
+    void update(ll x, ll y) {
+        update(1, 0, n - 1, x, y);
+    }
 };
